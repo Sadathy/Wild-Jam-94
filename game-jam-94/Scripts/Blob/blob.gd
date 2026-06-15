6fd850851dfc3b04fd780b8256ad8dde31527d72
duskrynn <liberty.smart@gmail.com>
2026-06-16 00:31:07 +0100

    Blobs move dynamically with velocity
    Updated the blob shader and added a script to blob.gd that gets the velocity of the blob.

--- game-jam-94/Scripts/Blob/blob.gd
+++ game-jam-94/Scripts/Blob/blob.gd
@@ -1,1 +1,20 @@
 extends CharacterBody2D
+
+var acceleration: float = 0.5
+var stopping_speed: float = 1.0 
+var shader_velocity := Vector2.ZERO
+
+func _ready() -> void:
+	#duplicates the material so it's unique for each blob :)
+	get_node("Blob_Base").material = get_node("Blob_Base").material.duplicate()
+
+func _physics_process(delta: float) -> void:
+	
+	if velocity.length() < 1.0:
+		acceleration = stopping_speed
+	
+	#changes acceleration to stopping smoothly
+	shader_velocity = shader_velocity.lerp((velocity/300), acceleration * delta)
+	
+	#sends velocity info to shader
+	get_node("Blob_Base").material.set_shader_parameter("blob_velocity", shader_velocity)
